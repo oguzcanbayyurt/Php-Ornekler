db3f891bb6220ec5476626c10259367b61c2da07
Oğuzcan Bayyurt <oguzcan.bayyurt@gmail.com>
2017-11-20 17:04:35 +0300

    Initial

---
+++ MySQL/MySQL - Create Database.php
@@ -0,0 +1,16 @@
+﻿<html>
+<?php
+$baglan=@mysql_connect("localhost","root","") or die ("baglanti hatasi");
+$sql="create database bil default character set utf8";;
+$sorgu=mysql_query($sql,$baglan);
+if(isset($sorgu))
+{
+	echo "veritabanı oluşturuldu";
+}
+else
+{
+	echo "veritabanı oluşturulmadı";
+}
+mysql_close($baglan);
+?>
+</html>
