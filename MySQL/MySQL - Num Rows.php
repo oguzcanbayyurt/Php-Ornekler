458684d00e1e63df37d27e4b15941bea02261a6a
Oğuzcan Bayyurt <oguzcan.bayyurt@gmail.com>
2017-11-20 17:05:20 +0300

    Initial

---
+++ MySQL/MySQL - Num Rows.php
@@ -0,0 +1,12 @@
+﻿<html>
+<?php
+$baglan=@mysql_connect("localhost","root","") or die ("baglanti hatasi");
+$sec=@mysql_select_db("cmyo",$baglan);
+$sorgu=mysql_query("select * from tablo1",$baglan);
+if(isset($sec))
+{
+	echo "veritabanına bağlandı ".mysql_num_rows($sorgu)." kayıt bulunmustur";
+}
+mysql_close($baglan);
+?>
+</html>
