c11a012d69b3ef988a1ae9c85ec183c952750103
Oğuzcan Bayyurt <oguzcan.bayyurt@gmail.com>
2017-11-20 17:06:05 +0300

    Initial

---
+++ MySQL/MySQL -Insert/insert.php
@@ -0,0 +1,30 @@
+﻿<html>
+<?php
+if($_POST)
+{
+	$ad=$_POST['ad'];
+	$soyad=$_POST['soyad'];
+	$cinsiyet=$_POST['cinsiyet'];
+	$baglan=@mysql_connect("localhost","root","") or die("bağlantı hatası");
+	$sec=@mysql_select_db("cmyo",$baglan) or die("veritabanı secilemedi");
+	mysql_query("set names'UTF8'");
+	$ekle=mysql_query("insert into tablo1(adi,soyadi,cinsiyet) values('.$ad.','.$soyad.','.$cinsiyet.')");
+	if($ekle)
+	{
+		echo "kayıt eklendi";
+	}
+	else
+	{
+		echo "kayıt eklenmedi";
+	}
+	echo "<br><br><br><u>eklenen bilgiler</u><br><br><br>";
+	echo "ad :".$ad."<br>";
+	echo "soyad :".$soyad."<br>";
+	echo "cinsiyet :".$cinsiyet."<br>";
+	echo "<br><a href='insert.html'>Geri Dön</a>";
+	
+}
+
+
+?>
+</html>
